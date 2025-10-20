bf3f53a63cc091bb9b62b4977e1a8a22d79d6f1b
Gabriel Holubowicz <gholubowicz@BIATEB.LAN>
2025-10-20 10:03:39 +0200

    res i req

--- index.js
+++ index.js
@@ -6,8 +6,23 @@ const PORT = 8080;
 app.get("/", (req, res)=>{
     res.send("Mój pierwszy program");
 })
-app.get("/pierwsza", (req, res)=>{
-    res.send("Mój pierwszy program i pierwsza strona");
+app.get("/status", (req, res)=>{
+    res.status(404).send("Mój pierwszy program");
+})
+app.get("/json", (req, res)=>{
+    res.json({"text":"to jest json"});
+})
+app.get("/body", (req, res)=>{
+    res.json({received:req.body});
+})
+app.get("/hello/:name",(req, res)=>{
+    res.send(`Cześć, ${req.params.name}`)
+})
+
+app.get("/samochod",(req, res)=>{
+    const year = req.query.rok;
+    const color = req.query.kolor;
+    res.send(`Cześć, szukam samochodu z rocznika ${year}, kolor ma być: ${color}`)
 })
 
 app.listen(PORT, (error)=>{
